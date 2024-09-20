5e4a09a22a697698defb7cba13f24674c8781da0
Tharaka Rupasinghe <140690160+TDR-VOID@users.noreply.github.com>
2024-09-20 21:16:41 +0530

    PP: done

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #include "SD.h" // SD Cards
 #include "SPI.h" // Serial Peripheral Interface
 
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+void Task1code( void * pvParameters );
+void Task2code( void * pvParameters );
+
+
 void writeFile(fs::FS &fs, const char * path, const char * message, bool append = false){
   Serial.printf("Writing file: %s\n", path);
 
@@ -29,6 +37,31 @@ void writeFile(fs::FS &fs, const char * path, const char * message, bool append 
 
 void setup(){
   Serial.begin(115200);
+
+
+  xTaskCreatePinnedToCore(
+    Task1code, /* Task function. */
+    "Task1",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task1,    /* Task handle to keep track of created task */
+    0);        /* pin task to core 0 */
+
+
+  xTaskCreatePinnedToCore(
+    Task2code, /* Task function. */
+    "Task2",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task2,    /* Task handle to keep track of created task */
+    1);        /* pin task to core 1 */
+
+
+
+
+
   if(!SD.begin(5)){ // CS pin - 5 (SPI communication)
     Serial.println("Card Mount Failed");
     return;
@@ -56,12 +89,26 @@ void setup(){
 
 }
 
-void loop(){
-for (int i = 0; i < 10; i++) {
-  Serial.println(i);
-  String WriteStr = "Hello" + String(i) + "\n"; 
-  writeFile(SD, "/hello.txt", WriteStr.c_str(), true);
-  delay(1000);
+void Task1code(void * pvParameters) {
+  while (1) {  // Run indefinitely
+    for (int i = 0; i < 10; i++) {
+      Serial.println(i);
+      String WriteStr = "Hello " + String(i) + "\n";
+      writeFile(SD, "/hello.txt", WriteStr.c_str(), true);
+      delay(1000);
+    }
+  }
 }
+
+
+void Task2code(void * pvParameters) {
+  while (1) {  // Run indefinitely
+    for (int j = 2; j < 14; j++) {
+      Serial.println(j);
+      String WriteStr = "Hello " + String(j) + "\n";
+      writeFile(SD, "/hi.txt", WriteStr.c_str(), true);
+      delay(1000);
+    }
+  }
 }
 
